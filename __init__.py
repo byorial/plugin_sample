43e841fff0d2d4ebcf86e0a4980754376c531187
byorial <orial@orial.kr>
2021-05-21 19:37:42 +0900

    .

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from .plugin import P
-
-blueprint = Blueprint(package_name, package_name, url_prefix='/%s' %  package_name, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+blueprint = P.blueprint
 menu = P.menu
 plugin_load = P.logic.plugin_load
 plugin_unload = P.logic.plugin_unload
